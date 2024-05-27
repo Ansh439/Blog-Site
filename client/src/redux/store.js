e49c7fbbe696d188cb1280b9294529963c61aa61
Ansh439 <gupta.ansh3150@gmail.com>
2024-05-27 09:02:02 +0530

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,26 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import {persistReducer, persistStore} from 'redux-persist' //redux persist to store info in local storage so that signin info will be kept even after refreshing
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    user: userReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig , rootReducer);  
+
 
 export const store = configureStore({
-    reducer: { 
-        user: userReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+        {serializableCheck: false,}
+    )
 })
+
+export const persistor = persistStore(store)
