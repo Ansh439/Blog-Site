ccbeba6ca8e1443d4dcc64f8a1c0a3a585c0acfc
Ansh439 <gupta.ansh3150@gmail.com>
2024-06-05 23:23:10 +0530

    Created User delete API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyUserToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 router.get('/test', test);
 router.put('/update/:userId', verifyUserToken, updateUser);
+router.delete('/delete/:userId', verifyUserToken, deleteUser);
 
 export default router;
