6fd842f34b0a23e0239e9f2aa96b7f9b5007f61d
Ansh439 <gupta.ansh3150@gmail.com>
2024-06-16 18:51:01 +0530

    added Show-More functionality in the post results of dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function DashPosts() {
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   const {currentUser} =  useSelector(state => state.user);
   console.log(userPosts);
   useEffect(() => {
@@ -14,6 +15,9 @@ export default function DashPosts() {
         const data = await res.json()
         if(res.ok){
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.nessage);
@@ -24,6 +28,21 @@ export default function DashPosts() {
     }
   }, [currentUser._id])
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className='overflow-x-scroll table-auto md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -73,6 +92,9 @@ export default function DashPosts() {
                 ))
               }
             </Table>
+            {
+              showMore && <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show More</button>
+            }
           </>
         ) : (
           <p> You have no posts yet! </p>
