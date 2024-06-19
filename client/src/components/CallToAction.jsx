c6bec04b1802264ecc75f95ae9ef795feabf66d6
Ansh439 <gupta.ansh3150@gmail.com>
2024-06-19 15:54:40 +0530

    added call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,23 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className='flex-1 justify-center flex flex-col'>
+            <h2 className='text-2xl'>
+                Want to learn more about Js?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Checkout this resources with 100 JS Projects
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href='https://www.100jsprojects.com' target='_blank' rel='noopener noreferrer'>100 JS Projects</a>
+            </Button>
+        </div>
+        <div className='p-7 flex-1'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1DmLCy9PSJfFqO55mNTYOQLx3x8THsbokkw&s' />
+        </div>
+    </div>
+  )
+}
